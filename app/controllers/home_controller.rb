160385901949c64cf1c1b935b9e89bc2809a24fa
Eugen Rochko <eugen@zeonfederated.com>
2016-10-24 14:05:15 +0200

    Display count of users on frontpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @twitter_count  = Authorization.where(provider: :twitter).count
+    @mastodon_count = Authorization.where(provider: :mastodon).count
   end
 end
